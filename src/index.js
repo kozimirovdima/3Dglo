c61ef6e6881bf51f32e8cdbf1e0b5baaa5f59f4c
kozimirovdima <kozimirovdima23@gmail.com>
2024-04-22 13:55:37 +0300

    changes lesson menu

--- src/index.js
+++ src/index.js
@@ -1,7 +1,8 @@
 import timer from "./modules/timer";
 import menu from "./modules/menu";
-
+import modal from "./modules/modal";
 console.log("index.js");
 timer("26 June 2024");
 
 menu();
+modal();
