028b17341c5631a3abe1a2d13a57773c1d3b21cc
kozimirovdima <kozimirovdima23@gmail.com>
2024-04-22 13:58:44 +0300

    lesson21validator

--- src/index.js
+++ src/index.js
@@ -1,8 +1,10 @@
 import timer from "./modules/timer";
 import menu from "./modules/menu";
 import modal from "./modules/modal";
+import validator from "./modules/validator";
 console.log("index.js");
 timer("26 June 2024");
 
 menu();
 modal();
+validator();
---
+++ src/modules/validator.js
@@ -0,0 +1,53 @@
+const validator = () => {
+  console.log("validator");
+  //Выделение полей в разделе "Расчитать стоимость"
+  const items = document.querySelectorAll(".calc-item");
+  // console.log(items)
+  let index;
+  //Отсекаем из Nodelist'a items поле "Интерьер квартиры" с индектом =0 и перебираем только 3 оставшихся поля, поочередно закладывая их в переменную itemCalc
+  for (index = 1; index < items.length; ++index) {
+    let itemCalc = items[index];
+    // console.log(itemCalc);
+    //Устанавливаем разрешение на ввод в поля только цифр
+    itemCalc.addEventListener("input", (e) => {
+      e.target.value = e.target.value.replace(/\D+/g, "");
+    });
+  }
+
+  //Только КИРИЛИЦА -Валидация поля имени в форме вначале, внизу и модальном окне
+  const userName = document.querySelectorAll('[name="user_name"]');
+
+  userName.forEach((element) => {
+    element.addEventListener("input", (e) => {
+      e.target.value = e.target.value.replace(/[^\-а-яА-ЯёЁ\s]+$/g, "");
+    });
+  });
+
+  //Валидация поля с e-mail
+  const email = document.querySelectorAll(".form-email");
+
+  email.forEach((el) => {
+    el.addEventListener("input", (e) => {
+      e.target.value = e.target.value.replace(
+        /([^A-Za-z0-9@\-\_\.\~\*\'])+$/g,
+        ""
+      );
+    });
+  });
+
+  //валидация телефона
+  const tel = document.querySelectorAll('input[type="tel"]');
+
+  tel.forEach((elem) => {
+    elem.addEventListener("input", (e) => {
+      e.target.value = e.target.value.replace(/[^0-9\(\)\-]/g, "");
+    });
+  });
+
+  //Только КИРИЛЛИЦА - валидация поля "Ваше сообщение в конце страницы"
+  const place = document.querySelector('[placeholder="Ваше сообщение"]');
+  place.addEventListener("input", (e) => {
+    e.target.value = e.target.value.replace(/[^\-а-яА-ЯёЁ\s]+$/g, "");
+  });
+};
+export default validator;
