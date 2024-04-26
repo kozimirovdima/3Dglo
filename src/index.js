2cf7605dd794d60deced2fc939d07dbe6b1ef83c
kozimirovdima <kozimirovdima23@gmail.com>
2024-04-26 11:24:25 +0300

    lesson22

--- src/index.js
+++ src/index.js
@@ -2,9 +2,11 @@ import timer from "./modules/timer";
 import menu from "./modules/menu";
 import modal from "./modules/modal";
 import validator from "./modules/validator";
+import tabs from "./modules/tabs";
 console.log("index.js");
 timer("26 June 2024");
 
 menu();
 modal();
 validator();
+tabs();
--- src/modules/modal.js
+++ src/modules/modal.js
@@ -1,17 +1,20 @@
 const modal = () => {
+  console.log("modal");
   const modal = document.querySelector(".popup");
   const buttons = document.querySelectorAll(".popup-btn");
-  const closeBtn = document.querySelector(".popup-close");
 
+  //
   let count = 0; //переменная для счетчика в цикле анимациии
 
   let idInterval; //переменная для запуска и остановки анимации
 
   //Функция-цикл для анимации параметра прозрачности "style.opacity" от 0 до 1 с шагом 0.01 (100 циклов) + запрет анимации, если ширина экрана (innerWidth)  меньше 768px
   const popupAnimate = () => {
-    console.log(window.innerWidth);
+    //Вывод ширины окна
+    // console.log(window.innerWidth)
     count++;
     idInterval = requestAnimationFrame(popupAnimate);
+    //Задаем исходные условия работы анимации (интервал увеличения видимости + ширина окна д.б более 768(не мобильный телефон))
     if (count < 100 && window.innerWidth > 768) {
       modal.style.opacity = count * 0.01;
     } else {
@@ -24,9 +27,15 @@ const modal = () => {
       (modal.style.display = "block"), popupAnimate();
     });
   });
-  // Навешиваем события на кнопку Х(закрытие окна) - меняем style.display = 'none'. Счетчик count обнуляем для возможности анимации, если после закрытия окна снова будет нажатие на кнопки в "Наши услуги"
-  closeBtn.addEventListener("click", () => {
-    (modal.style.display = "none"), (count = 0);
+
+  modal.addEventListener("click", (e) => {
+    e.preventDefault();
+    if (
+      !e.target.closest(".popup-content") ||
+      e.target.classList.contains("popup-close")
+    ) {
+      (modal.style.display = "none"), (count = 0);
+    }
   });
 };
 
