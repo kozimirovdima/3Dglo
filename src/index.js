1eb465d9c80052abdd52c8bc1118d3e1d493ab50
kozimirovdima <kozimirovdima23@gmail.com>
2024-05-08 11:35:17 +0300

    helpers

--- src/index.js
+++ src/index.js
@@ -1,15 +1,16 @@
 import timer from "./modules/timer";
 import menu from "./modules/menu";
-import modal from "./modules/modal";
+// import modal from './modules/modal'
+import modalk from "./modules/modalk";
 import validator from "./modules/validator";
 import tabs from "./modules/tabs";
 import slider from "./modules/slider";
 import calc from "./modules/calc";
 console.log("index.js");
-timer("26 June 2024");
-
+timer("28 february 2025");
 menu();
-modal();
+// modal();
+modalk();
 validator();
 tabs();
 slider();
